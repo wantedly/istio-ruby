70437171c2e3a5b806ccee25e34dcae9dfbbb4d0
bgpat <bgpat@users.noreply.github.com>
2018-05-25 15:23:29 +0900

    Remove rack middleware from context

--- lib/istio/tracing/context.rb
+++ lib/istio/tracing/context.rb
@@ -45,24 +45,5 @@ module Istio
 
     end
 
-    class RackMiddleware
-
-      def initialize(app)
-        @app = app
-      end
-
-      def call(env)
-        Context.build_current(extract_http_headers(env))
-        @app.call(env)
-      ensure
-        Context.remove_current
-      end
-
-      def extract_http_headers(env)
-        env.select{|k, v| k.start_with? 'HTTP_'}.map{|k, v| [k.gsub(/^HTTP_/, '').split('_').map(&:capitalize).join('-'), v]}.to_h
-      end
-
-    end
-
   end
 end
