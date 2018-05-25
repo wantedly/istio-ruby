d7e26691283c597085a99b6585ed3c33f4a33a0d
bgpat <bgpat@users.noreply.github.com>
2018-05-25 16:14:39 +0900

    Import railtie if rails loaded

--- lib/istio/tracing.rb
+++ lib/istio/tracing.rb
@@ -2,3 +2,14 @@ require "tracing/context"
 require "tracing/header"
 require "tracing/http"
 require "tracing/rack"
+
+begin
+  require 'rails/railtie'
+  rails_exists = true
+rescue LoadError
+  rails_exists = false
+end
+
+if rails_exists
+  require 'istio/tracing/railtie'
+end
