2dafed5e1d4b5b8d8b53c073d2f7629b2bc46b79
Sergey Moiseev <moiseev.sergey@gmail.com>
2018-12-31 15:43:13 +0200

    Cleanup

--- lib/ygrene_istio_tracing/tracing/rack.rb
+++ lib/ygrene_istio_tracing/tracing/rack.rb
@@ -17,7 +17,7 @@ module YgreneIstioTracing
 
       def extract_http_headers(env)
         env.select do |k, _v|
-          header = k.upcase.gsub(/^HTTP_/, '').gsub('_', '-')
+          header = k.upcase.gsub(/^HTTP_/, '').tr('_', '-')
           PROPAGATION_HEADERS.include?(header)
         end
       end
